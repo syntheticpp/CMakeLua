88b4b4c25dc279f589c89533177286706ca5845f
hoffman <devnull@localhost>
2008-01-01 22:19:11 +0000

    2008-01-01 22:19:11 by hoffman ENH: remove warning

--- Source/cmake.cxx
+++ Source/cmake.cxx
@@ -3,8 +3,8 @@
   Program:   CMake - Cross-Platform Makefile Generator
   Module:    $RCSfile: cmake.cxx,v $
   Language:  C++
-  Date:      $Date: 2008/01/01 20:13:41 $
-  Version:   $Revision: 1.347 $
+  Date:      $Date: 2008/01/01 22:19:11 $
+  Version:   $Revision: 1.348 $
 
   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
@@ -3822,7 +3822,7 @@ int cmake::VisualStudioLinkIncremental(std::vector<std::string>& args,
       {
       std::cout << "Create empty: " << manifestFile.c_str() << "\n";
       }
-    std::ofstream fout(manifestFile.c_str());
+    std::ofstream foutTmp(manifestFile.c_str());
     }
   std::string resourceFile = manifestFile;
   resourceFile += ".res";
