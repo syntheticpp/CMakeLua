cf9062e354cb1de861092a35aad8504b890547dc
alex <devnull@localhost>
2008-01-06 22:18:15 +0000

    2008-01-06 22:18:15 by alex BUG: fix #6105, if a directory inside CMAKE_FIND_ROOT_PATH is given to a FIND_XXX() command, don't prepend the root to it (since it is already in this root)
    Alex

--- Source/cmFindBase.cxx
+++ Source/cmFindBase.cxx
@@ -3,8 +3,8 @@
   Program:   CMake - Cross-Platform Makefile Generator
   Module:    $RCSfile: cmFindBase.cxx,v $
   Language:  C++
-  Date:      $Date: 2007/12/28 17:01:37 $
-  Version:   $Revision: 1.29 $
+  Date:      $Date: 2008/01/06 22:18:15 $
+  Version:   $Revision: 1.30 $
 
   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
@@ -536,8 +536,18 @@ void cmFindBase::HandleCMakeFindRootPath()
        it != unrootedPaths.end();
        ++it )
       {
-      std::string rootedDir=*rootIt;
-      rootedDir+=*it;
+      // if the current directory is already inside the current root, don't
+      // add the root again
+      std::string rootedDir;
+      if (cmSystemTools::IsSubDirectory(it->c_str(), rootIt->c_str()))
+        {
+        rootedDir = *it;
+        }
+      else
+        {
+        rootedDir=*rootIt;
+        rootedDir+=*it;
+        }
       this->SearchPaths.push_back(rootedDir);
       }
     }
