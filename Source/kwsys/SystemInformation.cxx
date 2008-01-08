5044603e3cb287f359aa9235a738e3c3967575cf
hoffman <devnull@localhost>
2008-01-08 19:59:57 +0000

    2008-01-08 19:59:57 by hoffman ENH: fix leaks and turn on by default

--- Source/kwsys/SystemInformation.cxx
+++ Source/kwsys/SystemInformation.cxx
@@ -3,8 +3,8 @@
   Program:   BatchMake
   Module:    $RCSfile: SystemInformation.cxx,v $
   Language:  C++
-  Date:      $Date: 2008/01/08 16:38:28 $
-  Version:   $Revision: 1.1 $
+  Date:      $Date: 2008/01/08 19:59:57 $
+  Version:   $Revision: 1.2 $
   Copyright (c) 2005 Insight Consortium. All rights reserved.
   See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
 
@@ -17,6 +17,7 @@
 #include KWSYS_HEADER(SystemInformation.hxx)
 #include KWSYS_HEADER(Process.h)
 #include KWSYS_HEADER(ios/iostream)
+#include KWSYS_HEADER(ios/sstream)
 #ifndef WIN32
   #include <sys/utsname.h> // int uname(struct utsname *buf);
 #endif
@@ -171,35 +172,35 @@ const char * SystemInformation::GetVendorID()
 }
 
 /** Return the type ID of the CPU */
-const char * SystemInformation::GetTypeID()
+kwsys_stl::string SystemInformation::GetTypeID()
 {
-  char * temp = new char[32];
-  sprintf(temp,"%d",this->ChipID.Type);
-  return temp;
+  kwsys_ios::ostringstream str;
+  str << this->ChipID.Type;
+  return str.str();
 }
 
 /** Return the family of the CPU present */
-const char * SystemInformation::GetFamilyID()
+kwsys_stl::string SystemInformation::GetFamilyID()
 {
-  char * temp = new char[32];
-  sprintf(temp,"%d",this->ChipID.Family);
-  return temp;
+  kwsys_ios::ostringstream str;
+  str << this->ChipID.Family;
+  return str.str();
 }
 
 // Return the model of CPU present */
-const char * SystemInformation::GetModelID()
+kwsys_stl::string SystemInformation::GetModelID()
 {
-  char * temp = new char[32];
-  sprintf(temp,"%d",this->ChipID.Model);
-  return temp;
+  kwsys_ios::ostringstream str;
+  str << this->ChipID.Model;
+  return str.str();
 }
 
 /** Return the stepping code of the CPU present. */
-const char * SystemInformation::GetSteppingCode()
-{
-  char * temp = new char[32];
-  sprintf(temp,"%d",this->ChipID.Revision);
-  return temp;
+kwsys_stl::string SystemInformation::GetSteppingCode()
+{ 
+  kwsys_ios::ostringstream str;
+  str << this->ChipID.Revision;
+  return str.str();
 }
 
 /** Return the stepping code of the CPU present. */
@@ -2649,7 +2650,8 @@ bool SystemInformation::QueryOSInformation()
       this->OSRelease = "Unknown";
       break;
   }
-
+  delete [] operatingSystem;
+  operatingSystem = 0;
 
   // Get the hostname
   WORD wVersionRequested;
