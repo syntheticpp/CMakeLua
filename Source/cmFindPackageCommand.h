46da2f8a89368d6b1e4a5e5a7aa9d8d0529de3a9
king <devnull@localhost>
2008-01-21 13:48:33 +0000

    2008-01-21 13:48:33 by king ENH: Implement version support in the find_package command module mode.  Version numbers provided to the command are converted to variable settings to tell the FindXXX.cmake module what version is requested.  This addresses issue #1645.

--- Source/cmFindPackageCommand.h
+++ Source/cmFindPackageCommand.h
@@ -3,8 +3,8 @@
   Program:   CMake - Cross-Platform Makefile Generator
   Module:    $RCSfile: cmFindPackageCommand.h,v $
   Language:  C++
-  Date:      $Date: 2008/01/17 14:02:31 $
-  Version:   $Revision: 1.16 $
+  Date:      $Date: 2008/01/21 13:48:33 $
+  Version:   $Revision: 1.17 $
 
   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
@@ -94,6 +94,11 @@ private:
   std::string CommandDocumentation;
   cmStdString Name;
   cmStdString Variable;
+  cmStdString Version;
+  unsigned int VersionMajor;
+  unsigned int VersionMinor;
+  unsigned int VersionPatch;
+  unsigned int VersionCount;
   cmStdString FileFound;
   bool Quiet;
   bool Required;
