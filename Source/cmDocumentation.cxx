3a67b1c63238b8fed0c4967c972cad6b454d9166
alex <devnull@localhost>
2008-02-17 19:04:01 +0000

    2008-02-17 19:04:01 by alex BUG: actually print the docs for custom modules if this was requested
    Alex

--- Source/cmDocumentation.cxx
+++ Source/cmDocumentation.cxx
@@ -3,8 +3,8 @@
   Program:   CMake - Cross-Platform Makefile Generator
   Module:    $RCSfile: cmDocumentation.cxx,v $
   Language:  C++
-  Date:      $Date: 2008/01/14 22:05:09 $
-  Version:   $Revision: 1.65 $
+  Date:      $Date: 2008/02/17 19:04:01 $
+  Version:   $Revision: 1.66 $
 
   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
@@ -1215,7 +1215,7 @@ bool cmDocumentation::PrintDocumentationCustomModules(std::ostream& os)
   this->ClearSections();
   this->CreateCustomModulesSection();
   this->AddSectionToPrint("Description");
-  this->AddSectionToPrint("Custom Modules");
+  this->AddSectionToPrint("Custom CMake Modules");
   this->AddSectionToPrint("Copyright");
   this->AddSectionToPrint("See Also");
 
