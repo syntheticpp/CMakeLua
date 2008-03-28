8c67f66c7c789773204f78f4aafb829be1fecd81
hoffman <devnull@localhost>
2008-03-28 19:59:18 +0000

    2008-03-28 19:59:18 by hoffman ENH: remove dangerous access to ivar that should not be used

--- Source/cmake.h
+++ Source/cmake.h
@@ -3,8 +3,8 @@
   Program:   CMake - Cross-Platform Makefile Generator
   Module:    $RCSfile: cmake.h,v $
   Language:  C++
-  Date:      $Date: 2008-03-22 14:24:05 $
-  Version:   $Revision: 1.110 $
+  Date:      $Date: 2008-03-28 19:59:18 $
+  Version:   $Revision: 1.111 $
 
   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
@@ -314,7 +314,6 @@ class cmake
    */
   const char* GetCTestCommand();
   const char* GetCPackCommand();
-  const char* GetCMakeCommand() const { return this->CMakeCommand.c_str(); }
 
   // Do we want debug output during the cmake run.
   bool GetDebugOutput() { return this->DebugOutput; }
