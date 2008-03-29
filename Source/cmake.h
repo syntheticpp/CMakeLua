b453709b90a2ad394177d91b70b5817b96eed84b
hoffman <devnull@localhost>
2008-03-29 17:16:02 +0000

    2008-03-29 17:16:02 by hoffman ENH: make sure gui no-dev workis

--- Source/cmake.h
+++ Source/cmake.h
@@ -3,8 +3,8 @@
   Program:   CMake - Cross-Platform Makefile Generator
   Module:    $RCSfile: cmake.h,v $
   Language:  C++
-  Date:      $Date: 2008-03-28 19:59:18 $
-  Version:   $Revision: 1.111 $
+  Date:      $Date: 2008-03-29 17:16:02 $
+  Version:   $Revision: 1.112 $
 
   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
@@ -347,7 +347,8 @@ class cmake
     }
   void SetSuppressDevWarnings(bool v)
     {
-      this->SuppressDevWarnings = v;
+      this->SuppressDevWarnings = v; 
+      this->DoSuppressDevWarnings = true;
     }
 
   /** Display a message to the user.  */
