8adae3f6e90d738b6913a99a3300800a2bf36524
martink <devnull@localhost>
2008-04-01 19:22:30 +0000

    2008-04-01 19:22:30 by martink COMP: fix warning

--- Source/cmGetPropertyCommand.cxx
+++ Source/cmGetPropertyCommand.cxx
@@ -3,13 +3,8 @@
   Program:   CMake - Cross-Platform Makefile Generator
   Module:    $RCSfile: cmGetPropertyCommand.cxx,v $
   Language:  C++
-<<<<<<< cmGetPropertyCommand.cxx
-  Date:      $Date: 2008/01/30 16:21:54 $
-  Version:   $Revision: 1.7 $
-=======
-  Date:      $Date: 2008-04-01 18:22:06 $
-  Version:   $Revision: 1.8 $
->>>>>>> 1.8
+  Date:      $Date: 2008-04-01 19:22:30 $
+  Version:   $Revision: 1.9 $
 
   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
@@ -171,7 +166,6 @@ bool cmGetPropertyCommand
   else if(this->InfoType == OutDefined)
     {
     // Lookup if the property is defined
-    const char *value;
     if(this->Makefile->GetCMakeInstance()->
        GetPropertyDefinition(this->PropertyName.c_str(), scope))
       {
