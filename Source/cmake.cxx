7759de6b6b796830bddd9b3e49a15a4e88d6d60c
alex <devnull@localhost>
2008-04-02 21:29:25 +0000

    2008-04-02 21:29:25 by alex ENH: make it possible to disable debug output again
    Alex

--- Source/cmake.cxx
+++ Source/cmake.cxx
@@ -3,8 +3,8 @@
   Program:   CMake - Cross-Platform Makefile Generator
   Module:    $RCSfile: cmake.cxx,v $
   Language:  C++
-  Date:      $Date: 2008-03-28 18:29:29 $
-  Version:   $Revision: 1.380 $
+  Date:      $Date: 2008-04-02 21:29:25 $
+  Version:   $Revision: 1.381 $
 
   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
@@ -618,7 +618,7 @@ void cmake::SetArgs(const std::vector<std::string>& args)
     else if(arg.find("--debug-output",0) == 0)
       {
       std::cout << "Running with debug output on.\n";
-      this->DebugOutputOn();
+      this->SetDebugOutputOn(true);
       }
     else if(arg.find("-G",0) == 0)
       {
--- Source/cmake.h
+++ Source/cmake.h
@@ -3,8 +3,8 @@
   Program:   CMake - Cross-Platform Makefile Generator
   Module:    $RCSfile: cmake.h,v $
   Language:  C++
-  Date:      $Date: 2008-03-29 17:16:02 $
-  Version:   $Revision: 1.112 $
+  Date:      $Date: 2008-04-02 21:29:25 $
+  Version:   $Revision: 1.113 $
 
   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
@@ -317,7 +317,7 @@ class cmake
 
   // Do we want debug output during the cmake run.
   bool GetDebugOutput() { return this->DebugOutput; }
-  void DebugOutputOn() { this->DebugOutput = true;}
+  void SetDebugOutputOn(bool b) { this->DebugOutput = b;}
 
   // Define a property
   void DefineProperty(const char *name, cmProperty::ScopeType scope,
