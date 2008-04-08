64388d0da8b0a2b6fc6993701cd0294b87dfe41e
hoffman <devnull@localhost>
2008-04-08 20:05:23 +0000

    2008-04-08 20:05:23 by hoffman ENH: half fix for 6688, don't let [ count go negative

--- Source/cmSystemTools.cxx
+++ Source/cmSystemTools.cxx
@@ -3,8 +3,8 @@
   Program:   CMake - Cross-Platform Makefile Generator
   Module:    $RCSfile: cmSystemTools.cxx,v $
   Language:  C++
-  Date:      $Date: 2008-04-08 17:42:30 $
-  Version:   $Revision: 1.371 $
+  Date:      $Date: 2008-04-08 20:05:23 $
+  Version:   $Revision: 1.372 $
 
   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
@@ -1270,7 +1270,10 @@ void cmSystemTools::ExpandListArgument(const std::string& arg,
         } break;
       case ']':
         {
-        --squareNesting;
+        if(squareNesting)
+          {
+          --squareNesting;
+          }
         newArgVec.push_back(*c);
         } break;
       case ';':
