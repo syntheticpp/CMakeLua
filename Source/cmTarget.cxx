ec1732607bda31d4681ab8ebe0817df4e14d2f3d
king <devnull@localhost>
2008-04-14 19:27:27 +0000

    2008-04-14 19:27:26 by king BUG: A per-config target name postfix should be ignored for Mac bundle and framework names.

--- Source/cmTarget.cxx
+++ Source/cmTarget.cxx
@@ -3,8 +3,8 @@
   Program:   CMake - Cross-Platform Makefile Generator
   Module:    $RCSfile: cmTarget.cxx,v $
   Language:  C++
-  Date:      $Date: 2008-04-08 04:06:46 $
-  Version:   $Revision: 1.214 $
+  Date:      $Date: 2008-04-14 19:27:26 $
+  Version:   $Revision: 1.215 $
 
   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
@@ -134,8 +134,8 @@ void cmTarget::DefineProperties(cmake *cm)
     ("DEBUG_POSTFIX", cmProperty::TARGET,
      "A postfix that will be applied to this target when build debug.",
      "A property on a target that specifies a postfix to add to the "
-     "target name when built in debug mode. For example foo.dll "
-     "versus fooD.dll");
+     "target name when built in debug mode. For example \"foo.dll\" "
+     "versus \"fooD.dll\".  Ignored for Mac Frameworks and App Bundles.");
 
   cm->DefineProperty
     ("EchoString", cmProperty::TARGET,
@@ -2311,6 +2311,12 @@ void cmTarget::GetFullNameInternal(TargetType type,
     std::string configProp = cmSystemTools::UpperCase(config);
     configProp += "_POSTFIX";
     configPostfix = this->GetProperty(configProp.c_str());
+    // Mac application bundles and frameworks have no postfix.
+    if(configPostfix &&
+       (this->IsAppBundleOnApple() || this->IsFrameworkOnApple()))
+      {
+      configPostfix = 0;
+      }
     }
   const char* prefixVar = this->GetPrefixVariableInternal(type, implib);
   const char* suffixVar = this->GetSuffixVariableInternal(type, implib);
